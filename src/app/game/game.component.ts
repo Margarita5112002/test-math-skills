faef7ca547ec75d15e6c0cb3a546103a74762a78
Margarita5112002 <margaritaalcarazrojas@gmail.com>
2025-03-17 18:52:43 -0700

    Add select a game screen

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-game',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
 export class GameComponent {
+    difficulty = 'mid'
+    mode = 'zen'
+
+    startGame() {
+        console.log(`difficulty ${this.difficulty}, mode ${this.mode}`)
+    }
 
 }
