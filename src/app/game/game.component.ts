a4e77d413301f5ac31071489a36d02433f80b3be
Margarita5112002 <margaritaalcarazrojas@gmail.com>
2025-03-17 20:48:15 -0700

    Add blitz and zen configuration

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
+import { Component, computed, signal, TemplateRef, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-game',
-  imports: [FormsModule],
+  imports: [FormsModule, NgTemplateOutlet ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
 export class GameComponent {
-    difficulty = 'mid'
-    mode = 'zen'
+    difficulty = signal('mid')
+    mode = signal('zen')
+    blitzTime = signal('3m')
+    zenNumberOfProblems = signal(10)
+    configBlitzTemplate = viewChild('configBlitz', { read: TemplateRef })
+    configZenTemplate = viewChild('configZen', { read: TemplateRef })
+    configTemplate = computed(() => {
+        const r = (this.mode() === 'zen' ? this.configZenTemplate() : this.configBlitzTemplate())
+        return r == undefined ? null : r
+    })
 
     startGame() {
-        console.log(`difficulty ${this.difficulty}, mode ${this.mode}`)
+        console.log(`difficulty ${this.difficulty()}, mode ${this.mode()}, blitzTime ${this.blitzTime()}, nProblem ${this.zenNumberOfProblems()}`)
     }
 
 }
