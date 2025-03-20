6ee24660fcc0efa1ee39468dc0987e1e208da82f
Margarita5112002 <margaritaalcarazrojas@gmail.com>
2025-03-19 21:26:33 -0700

    add jest and testing library

--- src/app/game-setup/game-setup.component.spec.ts
+++ src/app/game-setup/game-setup.component.spec.ts
@@ -1,23 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { render, screen } from '@testing-library/angular'
+import { GameSetupComponent } from './game-setup.component'
+import '@testing-library/jest-dom'
 
-import { GameSetupComponent } from './game-setup.component';
+describe('Game Setup', () => {
+    it('should render setup screen', async () => {
+        await render(GameSetupComponent)
 
-describe('GameSetupComponent', () => {
-  let component: GameSetupComponent;
-  let fixture: ComponentFixture<GameSetupComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [GameSetupComponent]
+        expect(screen.getByText('Select difficulty')).toBeVisible()
     })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(GameSetupComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+})
