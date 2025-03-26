5e88331b1ed6040bf093b9a14d5bce59e0498fc7
Margarita5112002 <margaritaalcarazrojas@gmail.com>
2025-03-25 18:54:13 -0700

    game display math problem

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,13 +1,15 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { GameSetupSettings } from '../interfaces/game-setup-settings';
-import { MathProblemDifficulty } from '../interfaces/math-problem';
+import { MathProblem, MathProblemDifficulty, MathProblemType } from '../interfaces/math-problem';
+import { MathProblemDisplayComponent } from "./math-problem-display/math-problem-display.component";
+import { MathProblemService } from '../math-problems/math-problem.service';
 
 @Component({
-  selector: 'app-game',
-  imports: [FormsModule],
-  templateUrl: './game.component.html',
-  styleUrl: './game.component.css'
+    selector: 'app-game',
+    imports: [FormsModule, MathProblemDisplayComponent],
+    templateUrl: './game.component.html',
+    styleUrl: './game.component.css'
 })
 export class GameComponent {
     gameSettings = input<GameSetupSettings>({
@@ -16,5 +18,19 @@ export class GameComponent {
         timeLimitMinutes: 3,
         numProblems: 10
     })
+    currMathProblem = signal<MathProblem>({
+        type: MathProblemType.ADD,
+        difficulty: MathProblemDifficulty.EASY,
+        operand1: 1,
+        operand2: 1,
+        solution: 2
+    })
+
+    constructor(private problemService: MathProblemService) {
+    }
+
+    ngOnInit() {
+        this.currMathProblem.set(this.problemService.getRandomMathProblem(this.gameSettings().difficulty))
+    }
 
 }
