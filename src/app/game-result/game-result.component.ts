fed56a47a4b65013b85e5f7c590f6bc0f9d163e7
Margarita5112002 <margaritaalcarazrojas@gmail.com>
2025-03-29 15:09:36 -0700

    game result shows all problems counters

--- src/app/game-result/game-result.component.ts
+++ src/app/game-result/game-result.component.ts
@@ -1,6 +1,18 @@
-import { Component, computed, input } from "@angular/core";
+import { Component, computed, input, Signal } from "@angular/core";
 import { GameResult } from "../interfaces/game-result";
-import { MathProblemDifficulty } from "../interfaces/math-problem";
+import { MathProblemDifficulty, MathProblemType } from "../interfaces/math-problem";
+
+interface ProblemCounter {
+    correct: number,
+    fail: number,
+    total: number,
+    correctPercentage: number
+}
+
+interface AllProblemCounters {
+    all: ProblemCounter,
+    types: Record<keyof typeof MathProblemType, ProblemCounter>
+}
 
 @Component({
     selector: 'game-result',
@@ -22,12 +34,54 @@ export class GameResultComponent {
             DIV: { correct: 10, fail: 10 },
         }
     })
-    totalCorrectProblems = computed(() => 
-        Object.values(this.gameResultInput().problemsCounter).reduce((p, c) => p + c.correct, 0)
-    )
-    totalProblems = computed(() => 
-        Object.values(this.gameResultInput().problemsCounter).reduce((p, c) => p + c.fail + c.correct, 0)
-    )
+    allProblemCountersVal: Signal<AllProblemCounters> = computed(() => {
+        const totMult = this.gameResultInput().problemsCounter.MULT.correct + this.gameResultInput().problemsCounter.MULT.fail
+        const totDiv = this.gameResultInput().problemsCounter.DIV.correct + this.gameResultInput().problemsCounter.DIV.fail
+        const totAdd = this.gameResultInput().problemsCounter.ADD.correct + this.gameResultInput().problemsCounter.ADD.fail
+        const totSub = this.gameResultInput().problemsCounter.SUB.correct + this.gameResultInput().problemsCounter.SUB.fail
+        const tot = totMult + totDiv + totSub + totAdd
+        const correctTot = Object.values(this.gameResultInput().problemsCounter).reduce((p, c) => p + c.correct, 0)
+
+        function roundTwoDecimals(n: number) {
+            return Math.round(n * 100) / 100
+        }
+
+        function makePercentage(c: number, t: number) {
+            if(t == 0) return 100
+            return roundTwoDecimals((c / t) * 100)
+        }
+
+        return {
+            all: {
+                correct: correctTot,
+                fail: tot - correctTot, 
+                total: tot, 
+                correctPercentage: makePercentage(correctTot, tot)
+            },
+            types: {
+                MULT: {
+                    ...this.gameResultInput().problemsCounter.MULT,
+                    total: totMult,
+                    correctPercentage: makePercentage(this.gameResultInput().problemsCounter.MULT.correct, totMult)
+                },
+                DIV: {
+                    ...this.gameResultInput().problemsCounter.DIV,
+                    total: totDiv,
+                    correctPercentage: makePercentage(this.gameResultInput().problemsCounter.DIV.correct, totDiv)
+                },
+                SUB: {
+                    ...this.gameResultInput().problemsCounter.SUB,
+                    total: totSub,
+                    correctPercentage: makePercentage(this.gameResultInput().problemsCounter.SUB.correct, totSub)
+                },
+                ADD: {
+                    ...this.gameResultInput().problemsCounter.ADD,
+                    total: totAdd,
+                    correctPercentage: makePercentage(this.gameResultInput().problemsCounter.ADD.correct, totAdd)
+                },
+            }
+        }
+    })
 
 
 }
